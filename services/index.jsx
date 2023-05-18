5231d6a32607a1aa656052eb48e2ccfaeca8022b
Venkata Sai <85883937+saitinku510@users.noreply.github.com>
2023-05-18 09:26:54 +0530

    Update index.jsx

--- services/index.jsx
+++ services/index.jsx
@@ -3,6 +3,6 @@ import axios from "axios";
 const BASE_URL = "https://dev-api.wall.app";
 
 export const fetchCampaignList = async (limit, tag, search) => {
-    const resp = await axios.get(`${BASE_URL}/api/v1/core/campaigns?limit=${limit}&offset=10&campaign_tag=${tag}&search=nft`)
+    const resp = await axios.get(`${BASE_URL}/api/v1/core/campaigns?limit=${limit}&offset=1&campaign_tag=${tag}&search=${search}`)
     return resp
 }
